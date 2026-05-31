3388f2ffce58b6384cdad891c4d540ec953826da
Azat Valiullin <144120963+awff1@users.noreply.github.com>
2026-05-31 16:12:11 +0300

    Delete src/main directory

--- src/main/java/com/itis/musiclabel/model/SubmissionStatus.java
+++
@@ -1,7 +0,0 @@
-package com.itis.musiclabel.model;
-
-public enum SubmissionStatus {
-    PENDING,
-    APPROVED,
-    REJECTED
-}
--- src/main/java/com/itis/musiclabel/repository/ProvidedServiceRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.itis.musiclabel.repository;
-
-import com.itis.musiclabel.model.ProvidedService;
-
-import java.util.List;
-
-public interface ProvidedServiceRepositoryCustom {
-    List<ProvidedService> findServicesWithFilters(String name, Long labelId, String sortBy);
-}
